ff61ecc1b8f15c90f0ec10136fe351cdbd1603e4
한진희 <0200134hjh@gmail.com>
2025-11-15 13:42:54 +0900

    Create Calculator.cs

---
+++ src/Calculator.cs
@@ -0,0 +1,29 @@
+namespace JavaLikeCsharp
+{
+    public class Calculator
+    {
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new System.Exception("Division by zero");
+            }
+            return a / b;
+        }
+    }
+}
